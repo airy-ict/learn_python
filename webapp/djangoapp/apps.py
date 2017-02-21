3b87042c8887aea6b851bda18d6c3318a89b6652
jreey <qdw8911@sina.com>
2017-02-21 15:22:29 +0800

    语言本地化

--- webapp/djangoapp/apps.py
+++ webapp/djangoapp/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class DjangositeConfig(AppConfig):
-    name = 'djangosite'
+    name = 'djangoapp'
--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""webapp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from djangosite import views
+from djangoapp import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
