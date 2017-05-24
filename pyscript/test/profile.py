4307e60cfa463cc479719c9fc74a4ecb9b746933
qi.dongwei <qi.dongwei@kjt.com>
2017-05-24 16:27:35 +0800

    曼哈顿距离实现  推荐排序

---
+++ pyscript/test/profile.py
@@ -0,0 +1,17 @@
+from time import time
+
+t = time()
+data = [
+    'a', 'b', 'is', 'python', 'jason', 'hello', 'hill', 'with', 'phone',
+    'test', 'dfdf', 'apple', 'pddf', 'ind', 'basic', 'none', 'baecr', 'var',
+    'bana', 'dd', 'wrd'
+]
+
+print(diclist)
+
+# dicr=[{dic[id]:({dp:dr})}  for (id,p,r) in users for (did,dp,dr) in users if id==did]
+# print(dic)
+
+# new_data=[]
+# [new_data.append(u) for x in data if len(x)>3]
+# print(new_data)
